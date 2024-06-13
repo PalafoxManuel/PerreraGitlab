46f84669b747af45f38c9c009584934094f7415d
MarioSal13 <mariosalmartinezren@gmail.com>
2024-06-13 12:06:50 -0700

    reporte ya no a medias

--- front/src/rutas/ReporteMaltrato.js
+++ front/src/rutas/ReporteMaltrato.js
@@ -1,58 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios'; // Importa Axios para las peticiones HTTP
 import Header from '../componentes/Header';
 import FormField from '../componentes/FormField';
-import Logo from '../img/Logo.png';
+import '../rutas/styles/Header.css'; // Ajuste de la ruta a '../styles/Home.css'
+import '../rutas/styles/Reporte.css'; // Importa los nuevos estilos
+import Logo from '../img/Logo.png'; // Ajusta la ruta según tu estructura de proyecto
 
 const uri_reporte = 'http://localhost:8000/reporte';
 const URI_MASCOTA = 'http://localhost:8000/mascota';
 const URI_USUARIO = 'http://localhost:8000/usuario';
 
 const Agregar = () => {
-  const [idTipoMascota, setIdTipoMascota] = useState('');
-  const [idUsuario, setIdUsuario] = useState('');
-  const [calleNumero, setCalleNumero] = useState('');
-  const [codigoPostal, setCodigoPostal] = useState('');
-  const [descripcionMaltrato, setDescripcionMaltrato] = useState('');
-  const [fechaReporte, setFechaReporte] = useState('');
-  const [usuarios, setUsuarios] = useState([]);
-  const [mascota, setMascota] = useState([]);
+  const [reportData, setReportData] = useState({
+    nombrePropietario: '',
+    nombreMascota: '',
+    calleNumero: '',
+    codigoPostal: '',
+    fechaReporte: '',
+    descripcionMaltrato: ''
+  });
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const responseUsuarios = await axios.get(URI_USUARIO);
-        setUsuarios(responseUsuarios.data);
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setReportData({ ...reportData, [name]: value });
+  };
 
-        const responseMascota = await axios.get(URI_MASCOTA);
-        setMascota(responseMascota.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
-
-    fetchData();
-  }, []);
-
-  const handleSubmit = async (event) => {
+  const handleFormSubmit = async (event) => {
     event.preventDefault();
-
     try {
-      const response = await axios.post(uri_reporte, {
-        Id_Tipo_Reporte: 2,
-        Id_Mascota: idTipoMascota,
-        Id_Usuario: idUsuario,
-        Contenido: `${calleNumero}, ${codigoPostal}, ${descripcionMaltrato}`,
-        Fecha_Reporte: fechaReporte
-      });
-
-      console.log('Reporte enviado:', response.data);
-
-      // Aquí podrías añadir lógica adicional después de enviar el reporte, por ejemplo, limpiar el formulario o mostrar un mensaje de éxito.
-      
+      const response = await axios.post(uri_reporte, reportData);
+      console.log('Reporte guardado exitosamente:', response.data);
+      // Aquí podrías redirigir a una página de éxito o hacer otra acción
     } catch (error) {
-      console.error('Error al generar reporte:', error);
-      // Aquí podrías manejar el error, mostrar un mensaje al usuario, etc.
+      console.error('Error al guardar el Reporte:', error);
     }
   };
 
@@ -68,60 +48,60 @@ const Agregar = () => {
               <img src={Logo} alt="Logo de huellita" className="logo" />
             </div>
             <hr />
-            <form onSubmit={handleSubmit}>
-              <FormField 
-                label="Nombre del propietario" 
-                type="select" 
-                name="idUsuario"
-                required={true}
-                options={usuarios.map(usuario => ({ value: usuario.Id_Usuario, label: usuario.Nombre_Usuario }))}
-                value={idUsuario}
-                onChange={(e) => setIdUsuario(e.target.value)}
+            <form onSubmit={handleFormSubmit}>
+              <FormField
+                label="Nombre del propietario"
+                type="text"
+                name="nombrePropietario"
+                value={reportData.nombrePropietario}
+                onChange={handleChange}
+                required
               />
-              <FormField 
-                label="Nombre de la mascota" 
-                type="select" 
-                name="idTipoMascota"
-                required={true}
-                options={mascota.map(mascota => ({ value: mascota.Id_Mascota, label: mascota.Nombre }))}
-                value={idTipoMascota}
-                onChange={(e) => setIdTipoMascota(e.target.value)}
+              <FormField
+                label="Nombre de la mascota"
+                type="text"
+                name="nombreMascota"
+                value={reportData.nombreMascota}
+                onChange={handleChange}
+                required
               />
-              <FormField 
-                label="Calle y número" 
-                type="text" 
+              <FormField
+                label="Calle y número"
+                type="text"
                 name="calleNumero"
-                value={calleNumero}
-                onChange={(e) => setCalleNumero(e.target.value)}
-                required={true}
+                value={reportData.calleNumero}
+                onChange={handleChange}
+                required
               />
-              <FormField 
-                label="Código Postal" 
-                type="text" 
+              <FormField
+                label="Código Postal"
+                type="text"
                 name="codigoPostal"
-                value={codigoPostal}
-                onChange={(e) => setCodigoPostal(e.target.value)}
-                required={true}
+                value={reportData.codigoPostal}
+                onChange={handleChange}
+                required
               />
-              <FormField 
-                label="Fecha del reporte" 
-                type="date" 
+              <FormField
+                label="Fecha del reporte"
+                type="date"
                 name="fechaReporte"
-                value={fechaReporte}
-                onChange={(e) => setFechaReporte(e.target.value)}
-                required={true}
+                value={reportData.fechaReporte}
+                onChange={handleChange}
+                required
               />
-              <FormField 
-                label="Descripción del maltrato" 
-                type="textarea" 
+              <FormField
+                label="Descripción del maltrato"
+                type="textarea"
                 name="descripcionMaltrato"
-                value={descripcionMaltrato}
-                onChange={(e) => setDescripcionMaltrato(e.target.value)}
-                required={true}
+                value={reportData.descripcionMaltrato}
+                onChange={handleChange}
+                required
               />
               <div className="form-buttons">
-                <button type="button" onClick={() => console.log('Cancelar')}>Cancelar</button>
-                <button type="submit" onClick={handleSubmit}>Guardar</button>
+                <button type="button" onClick={() => console.log('Cancelar')}>
+                  Cancelar
+                </button>
+                <button type="submit">Guardar</button>
               </div>
             </form>
           </div>
@@ -132,9 +112,3 @@ const Agregar = () => {
 };
 
 export default Agregar;
-
-
-
-
-
-
