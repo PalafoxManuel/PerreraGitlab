791efad7d1f8837e3c828109aa0864bb93d9292e
PalafoxManuel <mapa_21@alu.uabcs.mx>
2024-06-13 14:12:51 -0700

    Registro corregido 3

--- front/src/rutas/SignUp.js
+++ front/src/rutas/SignUp.js
@@ -66,7 +66,7 @@ const handleFormSubmit = async (e) => {
 
     if (clienteId) {
       await handleUserSubmit(clienteId); // Crea el usuario usando el ID del cliente
-      navigate('/LogIn'); // Redirige a la página Home después de registrar cliente y usuario
+      navigate('/'); // Redirige a la página Home después de registrar cliente y usuario
     } else {
       console.error('No se pudo obtener el ID del cliente.');
     }
