91ceda70aab21644dd4befaadf4bebbe54e4836a
PalafoxManuel <mapa_21@alu.uabcs.mx>
2024-06-14 09:42:56 -0700

    Correcion LoginAdmin2

--- front/src/rutas/AdminLogIn.js
+++ front/src/rutas/AdminLogIn.js
@@ -6,7 +6,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import './styles/LogIn.css';
 
-const URI_USUARIO = 'http://api.proyectounipedro.com/usuario';
+const URI_USUARIO = 'http://proyectounipedro.com/usuario';
 
 const AdminLogin = () => {
   const [username, setUsername] = useState('');
