417ba50fdd9126e168212dd8ff9b95add4b9d7f4
PalafoxManuel <136537184+PalafoxManuel@users.noreply.github.com>
2025-05-08 22:25:02 -0700

    Cerrar cesion corregido

--- resources/views/Components/Header.blade.php
+++ resources/views/Components/Header.blade.php
@@ -50,21 +50,21 @@
                 </ul>
             </li>
 
-            @auth
+            @if(session('usuario_id'))
             @if(session('perfil') === 'admin')
-            <li><a href="{{ route('admin.dashboard') }}">Panel Admin</a></li>
-            @elseif (auth()->user()->role === 'cliente')
-            <li><a href="{{ route('perfil') }}">Mi Perfil</a></li>
+                <li><a href="{{ route('usuarios.index') }}">Panel Admin</a></li>
+            @else
+                <li><a href="{{ route('usuarios.show', session('usuario_id')) }}">Mi Perfil</a></li>
             @endif
             <li>
                 <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-                    <button type="submit" class="btn-logout">Cerrar sesión</button>
+                @csrf
+                <button type="submit" class="btn-logout">Cerrar sesión</button>
                 </form>
             </li>
             @else
             <li><a href="{{ route('login') }}" class="btn-login">Iniciar sesión</a></li>
-            @endauth
+            @endif
         </ul>
     </div>
 
